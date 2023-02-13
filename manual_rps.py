306ce9e0fa3c6fbdb2190e7f2ee77fbd72eb4a12
amyth-singh <theamithsingh@gmail.com>
2023-02-13 16:52:46 +0000

    bug fix 3

--- manual_rps.py
+++ manual_rps.py
@@ -10,16 +10,13 @@ def get_user_choice():
   return item
 
 def get_winner(computer_choice, user_choice):
-  computer_choice = get_computer_choice()
-  if (user_choice == computer_choice):
-    print("It is a tie!")
-  elif (computer_choice == 'Rock') and (user_choice == 'Paper'):
-    print("You won!")
-  elif (computer_choice == 'Scissors') and (user_choice == 'Rock'):
-    print("You won!")
-  elif (computer_choice == 'Paper') and (user_choice == 'Scissors'):
-    print("You won!")
-  else:
-    print("You lost")
-
-get_winner('Rock', 'Paper')
+    if computer_choice == user_choice:
+        print('It is a tie!')
+    elif computer_choice == 'Rock' and user_choice == 'Paper':
+        print('You won!')
+    elif computer_choice == 'Paper' and user_choice == 'Scissors':
+        print('You won!')
+    elif computer_choice == 'Scissors' and user_choice == 'Rock':
+        print('You won!')
+    else:
+        print('You lost')
