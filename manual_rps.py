05a63724c83945bfd6165e39df56146dc3c3c14a
amyth-singh <theamithsingh@gmail.com>
2023-02-13 16:34:49 +0000

    bug fix 1

--- manual_rps.py
+++ manual_rps.py
@@ -1,7 +1,7 @@
 import random
 
 def get_computer_choice():
-    rpc = ['rock', 'paper', "scissors"]
+    rpc = ['Rock', 'Paper', 'Scissors']
     choice = random.choice(rpc)
     return choice
 
@@ -11,15 +11,15 @@ def get_user_choice():
 
 def get_winner(computer_choice, user_choice):
   computer_choice = get_computer_choice()
-  if (computer_choice == user_choice):
-    print("It is a tie!")
-  elif (computer_choice == "rock") and (user_choice == "paper"):
-    print("You won!")
-  elif (computer_choice == "scissors") and (user_choice == "rock"):
-    print("You won!")
-  elif (computer_choice == "paper") and (user_choice == "scissors"):
-    print("You won!")
+  if (user_choice == computer_choice):
+    print('It is a tie!')
+  elif (computer_choice == 'Rock') and (user_choice == 'Paper'):
+    print('You won!')
+  elif (computer_choice == 'Scissors') and (user_choice == 'Rock'):
+    print('You won!')
+  elif (computer_choice == 'Paper') and (user_choice == 'Scissors'):
+    print('You won!')
   else:
-    print("you lost")
+    print('you lost')
 
-get_winner("rock", "paper")
+get_winner('Rock', 'Paper')
