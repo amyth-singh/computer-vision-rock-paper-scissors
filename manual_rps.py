8590dcaad9662415d790eed6e7e7d10c7598a708
amyth-singh <theamithsingh@gmail.com>
2023-02-13 16:30:53 +0000

    added get_winner function

--- manual_rps.py
+++ manual_rps.py
@@ -8,3 +8,18 @@ def get_computer_choice():
 def get_user_choice():
   item = input("Input: ")
   return item
+
+def get_winner(computer_choice, user_choice):
+  computer_choice = get_computer_choice()
+  if (computer_choice == user_choice):
+    print("It is a tie!")
+  elif (computer_choice == "rock") and (user_choice == "paper"):
+    print("You won!")
+  elif (computer_choice == "scissors") and (user_choice == "rock"):
+    print("You won!")
+  elif (computer_choice == "paper") and (user_choice == "scissors"):
+    print("You won!")
+  else:
+    print("you lost")
+
+get_winner("rock", "paper")
