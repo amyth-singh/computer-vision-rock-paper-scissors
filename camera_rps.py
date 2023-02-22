366ce4de139b621865681d419d3820b61ef39556
amyth-singh <theamithsingh@gmail.com>
2023-02-22 18:51:08 +0000

    changes to camera.py file

--- camera_rps.py
+++ camera_rps.py
@@ -4,84 +4,78 @@ import time
 from keras.models import load_model
 import numpy as np
 
-# Press q to close the window
+class RPS():
+    def __init__(self, rounds_played=0, computer_wins=0, user_wins=0):
+        self.rps = ['Rock', 'Paper', 'Scissors']
+        self.model = load_model('keras_model.h5')
+        self.rounds_played = rounds_played
+        self.computer_wins = computer_wins
+        self.user_wins = user_wins
 
-def get_computer_choice(): # computer makes a random prediction
-    rpc = ['Rock', 'Paper', 'Scissors']
-    choice = random.choice(rpc)
-    return choice
+    def get_computer_choice(self):
+        choice = random.choice(self.rps)
+        return choice
 
-def get_prediction():
-    model = load_model('keras_model.h5')
-    cap = cv2.VideoCapture(0)
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-    
-    while True: 
-        ret, frame = cap.read()
-        resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
-        image_np = np.array(resized_frame)
-        normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
-        data[0] = normalized_image
-        prediction = model.predict(data)
-        cv2.imshow('frame', frame)
-    # Press q to close the window
-        print(prediction)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-        max_predict = np.max(prediction)        
-# After the loop release the cap object
-    cap.release()
-# Destroy all the windows
-    cv2.destroyAllWindows()
+    def get_prediction(self):
+        cap = cv2.VideoCapture(0)
+        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-computer_wins = [] # captures no of computer wins
-user_wins = [] # captures no of user wins
-rounds_played = []
+        while True: 
+            ret, frame = cap.read()
+            resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
+            image_np = np.array(resized_frame)
+            normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
+            data[0] = normalized_image
+            prediction = self.model.predict(data)
+            cv2.imshow('frame', frame)
+        # Press q to close the window
+            print(prediction)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            max_predict = np.max(prediction)
+            max_index = np.where(prediction == np.max(prediction))
+            prediction_index = max_index[1][0]
+    # After the loop release the cap object
+        cap.release()
+    # Destroy all the windows
+        cv2.destroyAllWindows()
+        print(prediction_index)
+        return prediction_index
 
-def get_winner(): #creates win logic
-    computer_choice = get_computer_choice()
-    user_choice = get_prediction()
+    def get_winner(self):
+        computer = self.get_computer_choice()
+        user = self.get_prediction()
+        if computer == self:
+            print('It is a tie!')
+        elif computer == 'Rock' and user == 1:
+            self.user_wins += 1
+            #self.computer_wins -= 1
+            print('You won!')
+        elif computer == 'Paper' and user == 2:
+            self.user_wins += 1
+            #self.computer_wins -= 1
+            print('You won!')
+        elif computer == 'Scissors' and user == 0:
+            self.user_wins += 1
+            #self.computer_wins -= 1
+            print('You won!')
+        else:
+            self.computer_wins += 1
+            #self.user_wins -= 1
+            print('You lost')
+        self.rounds_played += 1
 
-    if computer_choice == user_choice:
-        print('It is a tie!')
-    elif computer_choice == 'Rock' and user_choice == 'Paper':
-        user_wins.append('Win')
-        computer_wins.append('Lose')
-        print('You won!')
-    elif computer_choice == 'Paper' and user_choice == 'Scissors':
-        user_wins.append('Win')
-        computer_wins.append('Lose')
-        print('You won!')
-    elif computer_choice == 'Scissors' and user_choice == 'Rock':
-        user_wins.append('Win')
-        computer_wins.append('Lose')
-        print('You won!')
-    else:
-        user_wins.append('Lose')
-        computer_wins.append('Win')
-        print('You lost')
+    def play_game(self, rounds_played=0):
+        while True:
+            if self.user_wins == 3:
+                break
+            elif self.computer_wins == 3:
+                break
+            elif self.rounds_played == 5:
+                break
 
-def counter(): # creates a game counter
-  start = time.time()
-  wait = time.time() + 1
-  while time.time() < wait:
-    print(wait)
-  print("Chose Rock!")
-
-def winner_check():
-    if len(computer_wins) == 3:
-        print("Computer Wins!")
-    elif len(user_wins) == 3:
-        print("User Wins!")
-    elif len(rounds_played) == 5:
-        print("Game Over!")
-    else:
-        print("End")
-
-get_prediction()
-get_winner()
-counter()
-winner_check()
-print(get_computer_choice())
-print(computer_wins)
-print(user_wins)
+a = RPS()
+a.get_winner()
+print(a.computer_wins)
+print(a.user_wins)
+print(a.rounds_played)
